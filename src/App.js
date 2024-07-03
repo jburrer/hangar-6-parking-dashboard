346762ed32360ab58485db12bc760b1560b1afb7
Johnny Burrer <jburrer@purdue.edu>
2024-07-03 01:05:35 -0400

    rework in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import { useState } from 'react';
 
-function TitleBox() {
-    return (
-        <div id="title" className="box title-box"><h1>Aircraft Locations</h1></div>
-    );
+function titleCase(str) {
+    return str.toLowerCase().replace("-", " ").split(' ').map(w => w[0].toUpperCase() +
+           w.substring(1).toLowerCase()).join(' ');
 }
 
-function RampBox() {
+function RampBox({ spots, moveMode, toggleMoveMode }) {
     const rampBoxItems = [];
-    for (let i = 0; i < 18; i++) {
+    spots.forEach((spot) => {
         rampBoxItems.push(
-            <RampBoxItem n={i} />
+            <RampBoxItem spot={spot} moveMode={moveMode} toggleMoveMode={toggleMoveMode} />
         );
-    }
+    });
     return (
         <div id="ramp" className="box">
-            <div className="box title-box" style={{"fontSize": 1.5+"em"}}>Ramp</div>
+            <div className="box title-box" style={{"fontSize": 1.5+"em"}}>
+                Ramp
+            </div>
             <table className="table-list" border="1" frame="void" rules="rows">
                 <tbody>
                     {rampBoxItems}
@@ -26,56 +27,63 @@ function RampBox() {
     );
 }
 
-function RampBoxItem({ n }) {
-    let lowerCharStr = "ramp-" + String.fromCharCode(97 + n);
-    let upperChar = String.fromCharCode(65 + n);
+function RampBoxItem({ spot, moveMode, toggleMoveMode }) {
+    let upperChar = spot.name.replace("ramp-", "").toUpperCase();
+    let plane;
+    if (spot.plane) {
+        plane = (
+            <Plane plane={spot.plane} moveMode={moveMode} onClick={toggleMoveMode} />
+        );
+    }
     return (
-        <tr id={lowerCharStr} className="glowable">
+        <tr id={spot.name} className="glowable">
             <td>{upperChar}</td>
-            <td className="move-here"></td>
+            <td className="move-here">
+                {plane}
+            </td>
         </tr>
     );
 }
 
-function DirectionBox() {
+function DirectionBox({ spots }) {
     return (
         <div id="direction" className="box">
             <table className="center-table" border="1" frame="void">
                 <tbody>
                     <tr>
-                        <td id="xc-1" className="glowable" width="75px" height="148px">XC<div className="move-here many vert"></div></td>
+                        <DirectionElement name="xc-1" />
                         <td>
                             <table className="center-table" border="1" frame="void">
                                 <tbody>
                                     <tr>
-                                        <td id="kmcx" className="glowable" colSpan="2" height="70px">KMCX<div className="move-here many"></div></td>
+                                        <DirectionElement name="kmcx" />
                                     </tr>
                                     <tr>
-                                        <td id="northwest" className="glowable" height="70px">Northwest<div className="move-here many"></div></td>
-                                        <td id="northeast" className="glowable" height="70px">Northeast<div className="move-here many"></div></td>
+                                        <DirectionElement name="northwest" />
+                                        <DirectionElement name="northeast" />
                                     </tr>
                                 </tbody>
                             </table>
                         </td>
-                        <td id="xc-2" className="glowable" width="75px" height="148px">XC<div className="move-here many vert"></div></td>
+                        <DirectionElement name="xc-2" />
                     </tr>
                     <tr>
-                        <td id="xc-3" className="glowable" width="75px" height="148px">XC<div className="move-here many vert"></div></td>
+                        <DirectionElement name="xc-3" />
                         <td>
                             <table className="center-table" border="1" frame="void">
                                 <tbody>
                                     <tr>
-                                        <td id="southwest" className="glowable" height="70px">Southwest<div className="move-here many"></div></td>
-                                        <td id="southeast" className="glowable" height="70px">Southeast<div className="move-here many"></div></td>
+                                        <DirectionElement name="southwest" />
+                                        <DirectionElement name="southeast" />
                                     </tr>
                                     <tr>
-                                        <td id="kcfj" className="glowable" height="70px">KCFJ<div className="move-here many"></div></td>
-                                        <td id="kfkr" className="glowable" height="70px">KFKR<div className="move-here many"></div></td>
+                                        <DirectionElement name="kcfj" />
+                                        <DirectionElement name="kfkr" />
                                     </tr>
                                 </tbody>
                             </table>
                         </td>
-                        <td id="xc-4" className="glowable" width="75px" height="148px">XC<div className="move-here many vert"></div></td>
+                        <DirectionElement name="xc-4" />
                     </tr>
                 </tbody>
             </table>
@@ -83,7 +91,37 @@ function DirectionBox() {
     );
 }
 
-function HangarsBox() {
+function DirectionElement({ name }) {
+    let classNameVal = "move-here many";
+    let colSpanVal = 1;
+    let displayName;
+    let widthVal;
+    let heightVal;
+    if (name.length === 4) {
+        displayName = name.toUpperCase();
+    } else {
+        displayName = titleCase(name);
+    }
+    if (name.includes("xc")) {
+        displayName = "XC";
+        classNameVal += " vert";
+        widthVal = "75px";
+        heightVal = "148px";
+    } else {
+        heightVal = "70px";
+    }
+    if (name === "kmcx") {
+        colSpanVal = 2;
+    }
+    return (
+        <td id={name} className="glowable" width={widthVal} height={heightVal} colSpan={colSpanVal}>
+            {displayName}
+            <div className={classNameVal}></div>
+        </td>
+    );
+}
+
+function HangarsBox({ spots }) {
     return (
         <div id="hangars" className="box">
             <table className="center-table" border="1" frame="void">
@@ -94,67 +132,67 @@ function HangarsBox() {
                         <td colSpan="3" className="hangar-header">Hangar 6</td>
                     </tr>
                     <tr>
-                        <td id="h5-1" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td id="h5-2" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td id="h5-3" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td id="h6w-1" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h5-1" spotType="full" />
+                        <HangarsElement spotID="h5-2" spotType="full" />
+                        <HangarsElement spotID="h5-3" spotType="full" />
+                        <HangarsElement spotID="h6w-1" spotType="full" />
                         <td>
                             <table className="center-table" border="1" frame="void">
                                 <tbody>
                                     <tr>
-                                        <td id="h6w-7" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-7" spotType="half" />
                                     </tr>
                                     <tr>
-                                        <td id="h6w-8" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-8" spotType="half" />
                                     </tr>
                                 </tbody>
                             </table>
                         </td>
-                        <td id="h6w-4" className="glowable"><div className="move-here"></div></td>
-                        <td id="h6-1" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td id="h6-2" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td id="h6-3" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h6w-4" spotType="full" />
+                        <HangarsElement spotID="h6-1" spotType="full" />
+                        <HangarsElement spotID="h6-2" spotType="full" />
+                        <HangarsElement spotID="h6-3" spotType="full" />
                     </tr>
                     <tr>
-                        <td  id="h5-4" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td  id="h5-5" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td  id="h5-6" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
-                        <td  id="h6w-2" className="glowable"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h5-4" spotType="full" />
+                        <HangarsElement spotID="h5-5" spotType="full" />
+                        <HangarsElement spotID="h5-6" spotType="full" />
+                        <HangarsElement spotID="h6w-2" spotType="full" />
                         <td>
                             <table className="center-table" border="1" frame="void">
                                 <tbody>
                                     <tr>
-                                        <td id="h6w-9" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-9" spotType="half" />
                                     </tr>
                                     <tr>
-                                        <td id="h6w-10" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-10" spotType="half" />
                                     </tr>
                                 </tbody>
                             </table>
                         </td>
-                        <td id="h6w-5" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h6w-5" spotType="full" />
                         <td className="empty"></td>
-                        <td id="h6-4" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h6-4" spotType="full" />
                         <td className="empty"></td>
                     </tr>
                     <tr>
                         <td className="empty"></td>
-                        <td id="h5-7" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h5-7" spotType="full" />
                         <td className="empty"></td>
-                        <td id="h6w-3" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h6w-3" spotType="full" />
                         <td>
                             <table className="center-table" border="1" frame="void">
                                 <tbody>
                                     <tr>
-                                        <td id="h6w-11" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-11" spotType="half" />
                                     </tr>
                                     <tr>
-                                        <td id="h6w-12" className="glowable" width="75px" height="34px"><div className="move-here"></div></td>
+                                        <HangarsElement spotID="h6w-12" spotType="half" />
                                     </tr>
                                 </tbody>
                             </table>
                         </td>
-                        <td id="h6w-6" className="glowable" width="75px" height="68px"><div className="move-here"></div></td>
+                        <HangarsElement spotID="h6w-6" spotType="full" />
                         <td className="empty"></td>
                         <td className="empty"></td>
                         <td className="empty"></td>
@@ -165,61 +203,107 @@ function HangarsBox() {
     );
 }
 
-function OtherBox() {
+function HangarsElement({ spotID, spotType }) {
+    let heightVal = "68px";
+    if (spotType === "half") {
+        heightVal = "34px";
+    }
+    return (
+       <td id={spotID} className="glowable" width="75px" height={heightVal}>
+           <div className="move-here"></div>
+       </td>
+    );
+}
+
+function OtherBox({ spots }) {
+    const otherBoxItems = [];
+    Object.keys(spots).forEach((spot) => {
+        otherBoxItems.push(
+            <OtherBoxItem name={spot} />
+        );
+    });
     return (
         <div id="other" className="box">
-            <table id="other-table-list" className="table-list" border="1" frame="void" rules="rows">
+            <table id="other-table-list" className="table-list"
+                   border="1" frame="void" rules="rows">
                 <tbody>
-                    <tr id="approaches" className="glowable"><td>Approaches</td><td className="move-here many"></td></tr>
-                    <tr id="landings" className="glowable"><td>Landings</td><td className="move-here many"></td></tr>
-                    <tr id="purdue-aviation" className="glowable"><td>Purdue Aviation</td><td className="move-here many"></td></tr>
+                    {otherBoxItems}
                 </tbody>
             </table>
         </div>
     );
 }
 
-function TDoorBox() {
-    const tDoorBoxItems = [];
-    for (let i = 1; i <= 10; i++) {
-        tDoorBoxItems.push(
-            <TDoorBoxItem n={i} />
+function OtherBoxItem({ name }) {
+    let displayName = titleCase(name);
+    return (
+        <tr id={name} className="glowable">
+            <td>{displayName}</td>
+            <td className="move-here many"></td>
+        </tr>
+    );
+}
+
+function TDoorsBox({ spots }) {
+    const tDoorsBoxItems = [];
+    Object.keys(spots).forEach((spot) => {
+        tDoorsBoxItems.push(
+            <TDoorsBoxItem name={spot} />
         );
-    }
+    });
     return (
         <div id="t-doors" className="box">
             <div className="box title-box" style={{"fontSize": 1.5+"em"}}>T Doors</div>
             <table className="table-list" border="1" frame="void" rules="rows">
                 <tbody>
-                    {tDoorBoxItems} 
+                    {tDoorsBoxItems} 
                 </tbody>
             </table>
         </div>
     );
 }
 
-function TDoorBoxItem({ n }) {
-    let tString = "t-" + n;
+function TDoorsBoxItem({ name }) {
+    let n = name.replace("t-", "");
     return (
-        <tr id={tString} className="glowable">
+        <tr id={name} className="glowable">
             <td>{n}</td>
             <td className="move-here"></td>
         </tr>
     );
 }
 
-function PlaneBox() {
-
-    let planes = require("./planes.json");
+function PlaneBox({ planes }) {
     const planeElements = [];
     planes.forEach((plane) => {
         planeElements.push(
-            <Plane plane={plane} onPlaneClick={() => toggleMoveMode(plane.tailNumber)} />
+            <Plane plane={plane} />
         );
     });
+    return (
+        <div>
+            {planeElements}
+        </div>
+    );
+}
 
+function Plane({ plane, moveMode, toggleMoveMode }) {
+    let planeClass = "plane " + plane.type;
+    return (
+        <span id={plane.tailNumber} className={planeClass}
+              onClick={() => toggleMoveMode(plane.tailNumber)}>
+            {plane.tailNumber}
+        </span>
+    );
+}
+
+export default function App() {
+
+    // state
+    const [data, updateData] = useState(require("./data.json"));
     const [moveMode, setMoveMode] = useState(false);
 
+    // function to toggle mode for moving planes
     function toggleMoveMode(name, thisMoveMode = moveMode) {
         let plane = document.getElementById(name);
         if (thisMoveMode) {
@@ -250,40 +334,31 @@ function PlaneBox() {
 
     return (
         <div>
-            {planeElements}
-        </div>
-    );
-}
-
-function Plane({ plane, onPlaneClick }) {
-
-    let planeClass = "plane " + plane.type;
-
-    return (
-        <span id={plane.tailNumber} className={planeClass} onClick={onPlaneClick}>
-            {plane.tailNumber}
-        </span>
-    );
-
-}
-
-export default function App() {
-    return (
-        <div>
             <div id="frame">
-                <TitleBox /> 
-                <RampBox />
-                <DirectionBox />
-                <HangarsBox />
-                <OtherBox />
-                <TDoorBox />
+                <div id="title" className="box title-box">
+                    <h1>Aircraft Locations</h1>
+                </div>
+                <RampBox spots={data.spots.ramp}
+                    moveMode={moveMode}
+                    toggleMoveMode={toggleMoveMode} />
+                <DirectionBox spots={data.spots.direction}
+                    moveMode={moveMode}
+                    toggleMoveMode={toggleMoveMode} />
+                <HangarsBox spots={data.spots.hangars}
+                    moveMode={moveMode}
+                    toggleMoveMode={toggleMoveMode} />
+                <OtherBox spots={data.spots.other}
+                    moveMode={moveMode}
+                    toggleMoveMode={toggleMoveMode} />
+                <TDoorsBox spots={data.spots.tdoors}
+                    moveMode={moveMode}
+                    toggleMoveMode={toggleMoveMode} />
             </div>
             <div id="note">
                 Made with love by Johnny Burrer :)
                 Reach out on GroupMe with any feedback/bug reports!
             </div>
-            <PlaneBox /> 
+            <PlaneBox planes={data.planes} moveMode={moveMode} /> 
         </div>
     );
-
 }
